174508f503f0d3f06ce64322cc8e0e4b48850801
jesus014 <warner.santiago.03@gmail.com>
2022-06-24 13:09:47 -0500

    tomar el valor de la carta y los valores de la carta que sean letras convertirlos. Creacion del proyecto de cartas para utilizar los conceptos de js. creacion de script para poner en practica algunos fundamentos de js.

--- cartas/assets/js/juego.js
+++ cartas/assets/js/juego.js
@@ -1,9 +1,20 @@
-
-console.log('hola')
+//declaracion de variables globales
 let deck=[];
 const tipos=['C','D','H','S'];
 const especiales=['A','J','Q','K'];
 //esta funcion crea una nueva baraja
+let puntosJugador=0,
+    puntosComputadora=0;
+
+//referencias de html 
+const btnPedir = document.querySelector('#btnPedir');
+const divCartasJugador= document.querySelector('#jugador-cartas');
+const puntosHTML =document.querySelectorAll('small');
+//console.log(btnPedir);
+
+
+//se realiza la creacion del mazo de cartas aleatorio.
+
 const crearDeck = () => {
     for(let i=2 ; i <= 10; i++){
         //deck.push(i+'C');
@@ -34,8 +45,69 @@ const pedirCarta= ()=>{
     }
 
     const carta=deck.pop();
-    console.log(carta); //
+    //console.log(carta); //
     return carta;
 }
 
-pedirCarta();
+//pedirCarta();
+
+
+const valorCarta =(carta) =>{
+    const valor=carta.substring(0, carta.length-1);
+    // let puntos=0;
+    console.log(valor);
+    return (isNaN (valor) ) ?
+            (valor === 'A') ? 11 : 10
+            :valor * 1;
+                //logica que fue sustituida por operadores ternarios
+               //evalua si es un numero isNaN
+              // if(isNaN(valor)){
+               //
+              //     console.log('no es un numero')
+              //     puntos =(valor === 'A' ) ? 11 : 10;
+              // }else {
+              //     
+              //     console.log(' es un numero')
+              //     puntos = valor * 1;
+              // }
+              // console.log(puntos);
+
+}
+//se obtiene el valor de la carta para que se pueda utilizar al momento de que se llama
+//const valor= valorCarta(pedirCarta());
+
+//console.log({valor});
+
+//eventos
+//funcion callback es una funcion que se manda por argumento
+//se utiliza una funcion de flecha enves de function
+
+btnPedir.addEventListener('click',()=>{
+
+    const carta =pedirCarta();
+    
+    puntosJugador=puntosJugador+ valorCarta (carta) ;
+    console.log(puntosJugador);
+    puntosHTML[0].innerText=puntosJugador;
+
+    //crear la img deacuerdo con propiedades del DOM
+    const imgCarta=document.createElement('img');
+    //se le concatena el nombre 
+    imgCarta.src=`assets/cartas/${carta}.png`;
+    //se agrega la clase css
+    imgCarta.classList.add('carta');
+    divCartasJugador.append(imgCarta);
+
+    if(puntosJugador>21){
+
+        console.warn('lo siento perdiste');
+        btnPedir.disabled = true;
+
+    }else if(puntosJugador===21){
+        
+        console.warn(' 21');
+        btnPedir.disabled = true;
+        
+    }
+
+});
--- cartas/cartas.md
+++ cartas/cartas.md
@@ -1,6 +1,5 @@
 # Realizar proyecto con cartas, haciendo la logica con javascript.
 
-
 Se crea las carpetas correspondientes assets.
 
 Se utiliza boostrap para los estilos principales, como por botones.
@@ -8,3 +7,31 @@ Se utiliza boostrap para los estilos principales, como por botones.
 Se utiliza una libreria extra, la cual nos sirve para realizar los numeros aleatorios.
 
 https://underscorejs.org
+
+
+# DOM:
+
+ document object Model
+
+Se selecciona el documento bajo la direccion de document. 
+
+document.querySelector('small').innerHTML='`<b>`hola `</b>`'
+
+
+document.querySelector('.carta');
+
+document.getElementsByClassName('carta');
+
+
+**document**.**querySelector**(**'header'**).**innerText**=**'jesus'**;
+
+document.querySelector('.titulo').innerText='jesus';
+
+const titulo = document.querySelector('titulo');
+
+
+![1656085544739](image/cartas/1656085544739.png)
+
+![1656085661603](image/cartas/1656085661603.png)
+
+![1656085761466](image/cartas/1656085761466.png)
